00f6475ed12d6b5516c887d1a1ecbdd1e20db90a
Tavian Hall <tavian.hall@hotmail.com>
2021-10-31 21:17:54 -0500

    Everything

---
+++ app.js
@@ -0,0 +1,24 @@
+function show_result(str) {
+    "use strict";
+    if (window.XMLHttpRequest) {
+        xmlhttpreq = new XMLHttpRequest();
+    }
+    else { 
+        xmlhttpreq = new ActiveXObject(".XMLHTTP");
+    }
+    xmlhttpreq.onreadystatechange = function() { 
+        if (this.readyState == 4 && this.status==200) {
+            document.getElementById("Results").innerHTML=this.responseText;
+        }
+    }
+    xmlhttpreq.open("http://localhost/info2180-lab4/superheroes.php?q="+str,true);
+    xmlhttpreq.send();
+}
+
+window.onload = function () {
+    "use strict";
+    
+    document.getElementById("button").onclick = function () {
+        newresult(document.getElementById("text_Field").value)
+    }
+}
